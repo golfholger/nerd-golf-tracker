350d7c68398ae390a5ea7b2e03ec32925638d2dc
cza <cza@xps>
2019-12-10 12:36:28 +0100

    + ZwischenergebnisBefehl

---
+++ src/main/java/de/itagile/golf/befehl/ZwischenergebnisBefehl.java
@@ -0,0 +1,20 @@
+package de.itagile.golf.befehl;
+
+import de.itagile.golf.Befehl;
+import de.itagile.golf.operation.Zwischenergebnisausgabe;
+
+public class ZwischenergebnisBefehl implements Befehl {
+
+	public Zwischenergebnisausgabe operation() {
+		return new Zwischenergebnisausgabe();
+	}
+
+	public String kommando() {
+		return "Zwischenergebnis ausgeben";
+	}
+
+	public String beschreibung() {
+		return "dann gebe ich die Gesamtanzahl Schläge aus";
+	}
+
+}
