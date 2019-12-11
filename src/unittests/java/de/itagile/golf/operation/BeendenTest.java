19520d0868bf2088c25359702cf86a30c7c97ee3
Software Craftsman <csd@it-agile.de>
2019-12-11 14:59:02 +0100

    % adpated test

--- src/unittests/java/de/itagile/golf/operation/BeendenTest.java
+++ src/unittests/java/de/itagile/golf/operation/BeendenTest.java
@@ -1,16 +1,22 @@
 package de.itagile.golf.operation;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.Test;
 
+import de.itagile.golf.EinfacheScorecard;
 import de.itagile.golf.Operation;
-import de.itagile.golf.fehler.AnwendungSchliessenException;
+import de.itagile.golf.Scorecard;
 
 public class BeendenTest {
 
 	private Operation beenden = new Beenden();
 	
-	@Test(expected = AnwendungSchliessenException.class)
-	public void beendenTest() throws Exception {
-		beenden.fuehreAus(null);
+	@Test
+	public void beendenTest() {
+		Scorecard scorecard = new EinfacheScorecard();
+		scorecard.programmBeendenAusfuehren();
+		beenden.fuehreAus(scorecard);
+		assertThat(scorecard.programmBeendenPruefen(), equalTo(true));
 	}
 }
