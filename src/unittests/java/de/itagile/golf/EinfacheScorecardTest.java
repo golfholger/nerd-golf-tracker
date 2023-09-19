9c013fd696f4a24e3f4f2dc0866c151040659fa4
Timo Gerth <timo.gerth@rewe-digital.com>
2023-09-19 15:29:34 +0200

    feature(Lochwechsel): setze die schläge auf 0 wenn lochwechsel

--- src/unittests/java/de/itagile/golf/EinfacheScorecardTest.java
+++ src/unittests/java/de/itagile/golf/EinfacheScorecardTest.java
@@ -7,32 +7,33 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class EinfacheScorecardTest {
 
 	private Scorecard scorecard = new EinfacheScorecard();
-	
+
 	@Test
 	public void inkrementiertSchlagzahlEinmal() throws Exception {
 		scorecard.erhoeheAnzahlSchlaege();
 
 		assertThat(scorecard.anzahlSchlaege(), is(1));
 	}
-	
+
 	@Test
 	public void inkrementiertSchlagzahlMehrmals() throws Exception {
 		scorecard.erhoeheAnzahlSchlaege();
 		scorecard.erhoeheAnzahlSchlaege();
 		assertThat(scorecard.anzahlSchlaege(), is(2));
 	}
-	
+
 	@Test
 	public void setztSchlagzahlZurueck() throws Exception {
 		scorecard.erhoeheAnzahlSchlaege();
 		scorecard.schliesseLochAb();
+		assertThat(scorecard.anzahlSchlaege(), is(0));
 	}
-	
+
 	@Test
 	public void hatInitialErstesLoch() throws Exception {
 		assertThat(scorecard.aktuellesLoch(), is(1));
 	}
-	
+
 	@Test
 	public void zaehltAktuellesLoch() throws Exception {
 		scorecard.schliesseLochAb();
